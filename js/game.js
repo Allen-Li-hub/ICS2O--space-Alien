5919d84c1462a8ce1a8e185e20325bee610ddd49
Allen Li <113055658+Allen-Li-hub@users.noreply.github.com>
2022-11-10 14:05:28 -0500

    change

--- js/game.js
+++ js/game.js
@@ -1,15 +1,14 @@
-/**
- * 2022 Allen Li Space Alien game
- * Created by: Allen Li
- * Created on: Nov 2022
- */
+// Created by: Allen
+// Created on: Nov 2022
+
 
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a,
-};
 
-const game = new Phaser.Game(config);
-console.log(game);
+  backgroundColor: 0x5f6e7a,
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
