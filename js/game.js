6023a73ea0c453b44210b88aa55c6a640ff513da
Allen-Li-hub <Allen-Li-hub@users.noreply.github.com>
2022-11-10 19:07:28 +0000

    Prettified Code!

--- js/game.js
+++ js/game.js
@@ -1,14 +1,13 @@
 // Created by: Allen
 // Created on: Nov 2022
 
-
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
 
   backgroundColor: 0x5f6e7a,
-}
+};
 
-const game = new Phaser.Game(config)
-console.log(game)
+const game = new Phaser.Game(config);
+console.log(game);
