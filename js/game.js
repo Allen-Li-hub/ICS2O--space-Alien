9c215da82cf559ee9b5e28d8beb19c79c40d6c1d
Allen Li <113055658+Allen-Li-hub@users.noreply.github.com>
2022-11-15 14:23:32 -0500

    change

--- js/game.js
+++ js/game.js
@@ -1,8 +1,21 @@
-// Created by: Allen
+/* global Phaser */
+
+// Copyright (c) 2022 Allen Li
+//
+// Created by: Allen Li
 // Created on: Nov 2022
+// This is the Game Scene
+
+/**
+ * scene import statements
+ */
+
+import SplashScene from "./splashScene.js"
+
+const splashScene = new SplashScene()
 
 const config = {
-  type: Phaser.AUTO,
+  type: Phaser.ATUO,
   width: 1920,
   height: 1080,
   physics: {
@@ -17,7 +30,10 @@ const config = {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-};
+}
 
-const game = new Phaser.Game(config);
-console.log(game);
+const game = new Phaser.Game(config)
+
+game.scene.add("splashScene", splashScene)
+
+game.scene.start("splashScene")
---
+++ js/splashScene.js
@@ -0,0 +1,55 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Allen Li
+//
+// Created by: Allen Li
+// Created on: Nov 2022
+
+/**
+ * This class is the Splash Scene.
+ */
+
+
+class Splashscene extends Phaser.Scene {
+  
+  constructor() {
+    super({ key: "splashScene"})
+  }
+
+/**
+ * Can be defined on your own Scenes.
+ * This method is called by the Scene Manager when the scene starts,
+ *   before preload() and create() and create().
+ * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+ */
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff")
+  }
+
+  preload() {
+  console.log("Splash Scene")
+  }
+
+  /**
+   * can be defined on your own Scenes.
+   * Use it to create your game objects.
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
+   */
+
+  create(data) {
+  //pass
+  }
+
+  /**
+   * Should be overridden by your own Scenes.
+   * this method is called once per game step while the scene is running.
+   *  @param {number} time - The current time.
+   *  @param {number} delta - The delta time in ms since the last time.
+   */
+  update(time, delta) {
+    // pass
+  }
+}
+
+export default Splashscene
+
