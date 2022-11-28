c9fa3b025d20c526ffb25b5f7bdc21d4a2e84da3
Allen Li <113055658+Allen-Li-hub@users.noreply.github.com>
2022-11-28 14:29:24 -0500

    change

--- js/splashScene.js
+++ js/splashScene.js
@@ -38,6 +38,11 @@ class SplashScene extends Phaser.Scene {
    */
 
   create(data) {
+    this.splashSceneBackgroundImage = this.add.sprite(
+      0,
+      0,
+      "splashSceneBackground"
+    )
     this.splashSceneBackground = this.add.sprite(0, 0, "splashSceneBackground")
     this.splashSceneBackgroundImage.x = 1920 / 2
     this.splashSceneBackgroundImage.y = 1080 / 2
