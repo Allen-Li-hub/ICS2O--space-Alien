5bb58fb2211375a57b476759a05d151b42682ada
Allen-Li-hub <Allen-Li-hub@users.noreply.github.com>
2022-12-06 01:06:28 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -65,11 +65,16 @@ class GameScene extends Phaser.Scene {
    * Use it to create your game objects.
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
-  create (data) {
+  create(data) {
     this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
 
-    this.scoreText = this.add.text(10, 10, "Score: " + this.score.toString(), this.scoreTextStyle)
+    this.scoreText = this.add.text(
+      10,
+      10,
+      "Score: " + this.score.toString(),
+      this.scoreTextStyle
+    )
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
 
