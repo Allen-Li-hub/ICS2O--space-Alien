6d5e43462c9ac335359682e307de3494995da4bf
Allen Li <113055658+Allen-Li-hub@users.noreply.github.com>
2022-12-08 14:22:29 -0500

    change

--- js/gameScene.js
+++ js/gameScene.js
@@ -31,11 +31,8 @@ class GameScene extends Phaser.Scene {
     this.fireMissile = false
     this.score = 0
     this.scoreText = null
-    this.scoreTextStyle = {
-      font: "65px Arial",
-      fill: "#ffffff",
-      align: "center",
-    }
+    this.scoreTextStyle = { font: "65px Arial", fill: "ffffff", align: "center" }
+    this.gameOverTextStyle = { font: "65px Arial", fill: "ff0000", align: "center" }
   }
 
   /**
@@ -98,6 +95,17 @@ class GameScene extends Phaser.Scene {
         this.createAlien()
       }.bind(this)
     )
+
+    this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
+      this.sound.play("bomb")
+      this.physics.pause()
+      alienCollide.destroy()
+      shipCollide.distroy()
+      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again.", this.gameOverTextStyle).setOrigin(0.5)
+      this.gameOverText.setInteractive({ useHandCursor: true })
+      this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"))
+    }.bind(this))
+
   }
 
   /**
