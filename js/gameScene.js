992e308fa65bbf69f95b00981626e0c320461286
Allen Li <113055658+Allen-Li-hub@users.noreply.github.com>
2022-12-08 14:43:12 -0500

    change

--- js/gameScene.js
+++ js/gameScene.js
@@ -63,6 +63,7 @@ class GameScene extends Phaser.Scene {
 
     this.load.audio("laser", "./assets/laser1.wav")
     this.load.audio("explosion", "./assets/barrelExploding.wav")
+    this.load.audio("bomb", "./assets/bomb.wav")
   }
 
   /**
